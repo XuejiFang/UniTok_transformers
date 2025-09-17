b38446756f4ba834444a3174bba95c98ccef9e01
XuejiFang <jaggar.fang@hotmail.com>
2025-09-17 12:47:32 +0800

    update

--- inference.py
+++ inference.py
@@ -26,30 +26,26 @@ def main(args):
     unitok.to(device)
     unitok.eval()
 
-    for i  in range(8):
-        args.src_img = f'assets/vis_imgs/v{i}.jpg'
-        args.rec_img = f'./assets/rec_imgs/384-4/rec_v{i}.png'
+    img = Image.open(args.src_img).convert("RGB")
+    original_size = img.size
+    processor = MultiResolutionProcessor(max_size=getattr(args, 'max_resolution', 256), patch_size=16)
+    result = processor.process_image(img, training=False)
+    img_tensor = transforms.ToTensor()(result['image'])
+    img_tensor = normalize_01_into_pm1(img_tensor).unsqueeze(0).to('cuda')
 
-        img = Image.open(args.src_img).convert("RGB")
-        original_size = img.size
-        processor = MultiResolutionProcessor(max_size=getattr(args, 'max_resolution', 256), patch_size=16)
-        result = processor.process_image(img, training=False)
-        img_tensor = transforms.ToTensor()(result['image'])
-        img_tensor = normalize_01_into_pm1(img_tensor).unsqueeze(0).to('cuda')
-
-        with torch.no_grad():
-            rec_img = unitok.img_to_reconstructed_img(
-                img_tensor,
-                shape_info=result['shape_info'],
-                attention_mask=None  # set none for inference
-            )
-        
-        print(f'Original Image Size: {original_size} -> Resized To: {result["shape_info"]["processed_size"]}')
-        
-        if args.rec_img:
-            final_path = args.rec_img
-            save_img(rec_img, final_path)
-            print(f'Saved Reconstructed Image To: {final_path}')
+    with torch.no_grad():
+        rec_img = unitok.img_to_reconstructed_img(
+            img_tensor,
+            shape_info=result['shape_info'],
+            attention_mask=None  # set none for inference
+        )
+    
+    print(f'Original Image Size: {original_size} -> Resized To: {result["shape_info"]["processed_size"]}')
+    
+    if args.rec_img:
+        final_path = args.rec_img
+        save_img(rec_img, final_path)
+        print(f'Saved Reconstructed Image To: {final_path}')
 
 
 if __name__ == '__main__':
